7096928a0c0c7129d1936644b3aea0231e46bf71
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-09-21 11:16:00 -0500

    feat: :sparkles: agrego conexion db
    agrego la conexion a la db

---
+++ api/db/atlas.js
@@ -0,0 +1,21 @@
+import { loadEnv } from "vite";
+import { MongoClient } from "mongodb";
+
+const env = loadEnv("development", process.cwd(), 'ATLAS')
+
+const DB = JSON.parse(env.ATLAS_CONNECTION);
+
+export async function connectDB(){
+    try{
+        const URI = `mongodb+srv://${DB.user}:${DB.password}@cluster0.lfged0r.mongodb.net/${DB.database}`
+        const options = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        };
+        const client = await MongoClient.connect(URI,options);
+        console.log("Conectado");
+        return client.db();
+    } catch(error){
+        return {status:500,message:error}
+    }
+}
