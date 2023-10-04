b97ab1a47fe0c4f6a10caffe8c23105746f1e256
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-10-04 05:20:47 -0500

    feat: :sparkles: Agrego post de usuarios

---
+++ api/controllers/adminControllers.js
@@ -0,0 +1,33 @@
+import * as loginStaff from "../services/admin.js"
+import {validationResult} from "express-validator"
+import { generateToken } from "../jwt/jwt.js";
+
+//Registrar encargados
+export const signUpStaff = async(req,res)=>{
+    //ratelimit
+    console.log(req.rateLimit);
+    //Validacion 
+    const errors = validationResult(req); 
+    if (!errors.isEmpty()) return res.status(422).send(errors);
+    try {
+        let data = await loginStaff.signUpStaff(req.body);
+        let token = await generateToken(data)
+        res.status(200).json({
+            status:200,
+            message:"usuario creado exitosamente",
+            token,
+            data
+        })
+    } catch (error) {
+        if(error.code == 11000){ // es el error de usuarios repetidos de mongodb
+            res.status(409).json({
+                message:"El correo ya está en uso"
+            })
+        }
+        else{
+            res.status(500).json({
+                error:error.message
+            })
+        }
+    }
+}
